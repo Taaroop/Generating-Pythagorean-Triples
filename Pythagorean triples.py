6f0255a2d06f6e1ab3f5370078125a31f8772a8c
Taaroop <67786757+Taaroop@users.noreply.github.com>
2022-01-31 22:41:38 +0600

    Update Pythagorean triples.py

--- Pythagorean triples.py
+++ Pythagorean triples.py
@@ -1,13 +1,14 @@
+# Function
 
-def p_triples(a, b):
+def p_triples(a):
     superset = []
     li = []
-    for m in range(a, b+1):
-        for n in range(a, b+1):
+    for m in range(1, a+1):
+        for n in range(1, a+1):
             x = 2*m*n
             y = abs((m**2) - (n**2))
             z = (m**2) + (n**2)         # See notes for detailed mathematical explanations and derivation
-            if z > b:
+            if z > a:
                 return superset
             else:
                 li = [x, y, z]
@@ -16,3 +17,9 @@ def p_triples(a, b):
                 if li not in superset and x > 0 and y > 0 and z > 0:
                     superset.append(li)
     return superset
+
+# Testing
+
+end = int(input("Enter the boundary range of the Pythagorean triples: "))
+for item in p_triples(end):
+    print(item)
